c51543629a1303b320c829e04c9d36e4d300fe5c
Mengyang <mengyzhang87@gmail.com>
2019-06-11 11:25:25 -0400

    finished pointers and weather assignment

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -12,7 +12,19 @@ Weather::Weather(std::string nm, GPS loc) :
 }
 
 
-string Weather::get_name() {
-    return station_nm;
+string Weather::get_name() const { return station_nm; }
+
+int Weather::get_rating() const { return rating; }
+
+void Weather::set_rating(int new_rating) { rating = new_rating; }
+
+ostream& operator<<(ostream& os, const GPS& gps) {
+	os << "lat: " << gps.latitude << " long: " << gps.longitude;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Weather& w) {
+	os << w.get_name() << ", " << w.get_rating() << ", " << w.my_loc;
+	return os;
 }
 
